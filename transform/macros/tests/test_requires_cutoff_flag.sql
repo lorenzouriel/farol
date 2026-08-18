4770a4d5d097bf65f0916dfa869d29acf2656262
Lorenzo Uriel <lorenzouriel394@gmail.com>
2026-08-17 22:58:22 -0300

    feat: let's create the base for farol

--- transform/macros/tests/test_requires_cutoff_flag.sql
+++
@@ -1,5 +0,0 @@
-{% test requires_cutoff_flag(model) %}
-    select *
-    from {{ model }}
-    where janela_fechada is null
-{% endtest %}
--- transform/macros/tests/test_requires_legal_qualifiers.sql
+++
@@ -1,5 +0,0 @@
-{% test requires_legal_qualifiers(model) %}
-    select *
-    from {{ model }}
-    where status_processual is null or instancia is null
-{% endtest %}
